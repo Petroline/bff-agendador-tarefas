379f2721cffd2db4364d2fcae667944bb3c68c82
Everton <evertonpetroline@outlook.com>
2025-12-16 05:57:12 -0300

    adicionado esqueleto do projeto

---
+++ src/test/java/com/petroline/bffagendadortarefas/BffAgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petroline.bffagendadortarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffAgendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
