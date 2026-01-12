17e2583b15dc0b075cab89fda97701935819bc29
Everton <evertonpetroline@outlook.com>
2026-01-12 06:44:36 -0300

    adicionado Client de email

---
+++ src/main/java/com/petroline/bffagendadortarefas/business/EmailService.java
@@ -0,0 +1,20 @@
+package com.petroline.bffagendadortarefas.business;
+
+import com.petroline.bffagendadortarefas.business.dto.out.TarefasDTOResponse;
+import com.petroline.bffagendadortarefas.infrastructure.client.EmailClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final EmailClient emailClient;
+
+
+    public void enviaEmail(TarefasDTOResponse dto) {
+        emailClient.enviarEmail(dto);
+
+    }
+}
+
---
+++ src/main/java/com/petroline/bffagendadortarefas/infrastructure/client/EmailClient.java
@@ -0,0 +1,56 @@
+package com.petroline.bffagendadortarefas.infrastructure.client;
+
+import com.petroline.bffagendadortarefas.business.dto.in.EnderecoDTORequest;
+import com.petroline.bffagendadortarefas.business.dto.in.LoginRequestDTO;
+import com.petroline.bffagendadortarefas.business.dto.in.TelefoneDTORequest;
+import com.petroline.bffagendadortarefas.business.dto.in.UsuarioDTORequest;
+import com.petroline.bffagendadortarefas.business.dto.out.EnderecoDTOResponse;
+import com.petroline.bffagendadortarefas.business.dto.out.TarefasDTOResponse;
+import com.petroline.bffagendadortarefas.business.dto.out.TelefoneDTOResponse;
+import com.petroline.bffagendadortarefas.business.dto.out.UsuarioDTOResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+@FeignClient(name ="notificacao", url = "${notificacao.url}")
+public interface EmailClient {
+
+
+    @GetMapping
+    UsuarioDTOResponse buscaUsuarioPorEmail(@RequestParam("email") String email,
+                                            @RequestHeader("Authorization") String token);
+
+    @PostMapping
+    UsuarioDTOResponse salvaUsuario(@RequestBody UsuarioDTORequest usuarioDTO);
+
+    @PostMapping("/login")
+    String login(@RequestBody LoginRequestDTO usuarioDTO);
+
+    @DeleteMapping("/{email}")
+    void deletaUsuarioPorEmail(@PathVariable String email,
+                               @RequestHeader("Authorization") String token);
+
+    @PutMapping
+    UsuarioDTOResponse atualizaDadosUsuario(@RequestBody UsuarioDTORequest dto,
+                                            @RequestHeader("Authorization") String token);
+
+    @PutMapping("/endereco")
+    EnderecoDTOResponse atualizaEndereco(@RequestBody EnderecoDTORequest dto,
+                                         @RequestParam("id") Long id,
+                                         @RequestHeader("Authorization") String token);
+
+    @PutMapping("/telefone")
+    TelefoneDTOResponse atualizaTelefone(@RequestBody TelefoneDTORequest dto,
+                                         @RequestParam("id") Long id,
+                                         @RequestHeader("Authorization") String token);
+
+    @PostMapping("/endereco")
+    EnderecoDTOResponse cadastroEndereco(@RequestBody EnderecoDTORequest dto,
+                                         @RequestHeader("Authorization") String token);
+
+    @PostMapping("/telefone")
+    TelefoneDTOResponse cadastroTelefone(@RequestBody TelefoneDTORequest dto,
+                                         @RequestParam("Authorization") String token);
+
+    void enviarEmail(@RequestBody TarefasDTOResponse dto);
+
+}
